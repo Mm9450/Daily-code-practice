062da21f1460f574ded317cd7cbb8501bf25ad22
Mm9450 <manas1999mishra@gmail.com>
2022-01-17 16:51:10 +0530

    added

---
+++ Bitwise operator/Bitwise operator/Bitwise operator.cpp
@@ -0,0 +1,15 @@
+// Bitwise operator.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+	int a = 4;
+	int b = 6;
+
+	cout << a | b
+   
+}
